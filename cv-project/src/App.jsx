6d416b1fef931b7a106184982d0d1c129d243ebb
shyam sabu <shyam@techwyseintl.com>
2024-10-23 23:19:13 +0530

    first commit

---
+++ cv-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
